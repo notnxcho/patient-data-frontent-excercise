3ccf24be1f1103f9126b750559a825db57f4d512
Nacho Lorenzo <nacholorenzo.650@gmail.com>
2024-07-27 08:15:45 -0300

    fixed an error popping in the console regarding unrecognized prop

--- src/assets/icons/addIcon.tsx
+++ src/assets/icons/addIcon.tsx
@@ -12,23 +12,23 @@ const AddIcon = ({ color = 'black', stroke = 1.25, size = 16 }: IconProps) => {
       <path
         d='M11.3335 6.66667H13.3335M15.3335 6.66667H13.3335M13.3335 6.66667V4.66667M13.3335 6.66667V8.66667'
         stroke={color}
-        stroke-width={`${stroke}`}
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth={`${stroke}`}
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M0.666504 13.3333V12.6667C0.666504 10.0893 2.75584 8 5.33317 8V8C7.9105 8 9.99984 10.0893 9.99984 12.6667V13.3333'
         stroke={color}
-        stroke-width={`${stroke}`}
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth={`${stroke}`}
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
       <path
         d='M5.33317 8C6.80593 8 7.99984 6.80609 7.99984 5.33333C7.99984 3.86057 6.80593 2.66667 5.33317 2.66667C3.86041 2.66667 2.6665 3.86057 2.6665 5.33333C2.6665 6.80609 3.86041 8 5.33317 8Z'
         stroke={color}
-        stroke-width={`${stroke}`}
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth={`${stroke}`}
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   )
--- src/assets/icons/editIcon.tsx
+++ src/assets/icons/editIcon.tsx
@@ -12,9 +12,9 @@ const EditIcon = ({ color = 'black', stroke = 1.25, size = 16 }: IconProps) => {
       <path
         d='M8.68076 3.88562L10.5664 2L13.8662 5.29983L11.9806 7.18545M8.68076 3.88562L2.37399 10.1924C2.18646 10.3799 2.0811 10.6343 2.0811 10.8995V13.7851H4.96672C5.23193 13.7851 5.48629 13.6798 5.67382 13.4922L11.9806 7.18545M8.68076 3.88562L11.9806 7.18545'
         stroke={color}
-        stroke-width={`${stroke}`}
-        stroke-linecap='round'
-        stroke-linejoin='round'
+        strokeWidth={`${stroke}`}
+        strokeLinecap='round'
+        strokeLinejoin='round'
       />
     </svg>
   )
